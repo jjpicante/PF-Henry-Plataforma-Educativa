b07920aa162becef83cd8f0b935932596acedb74
pablo-salituri <pablo_salituri@live.com.ar>
2023-04-12 19:04:27 -0300

    cambié force por alter

--- Back/index.js
+++ Back/index.js
@@ -20,7 +20,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(3001, () => {
     console.log("%s listening at 3001");
   });
