6e0d4c3cb108aa6c90ef5d4531412f1d60967a98
pablo-salituri <pablo_salituri@live.com.ar>
2023-04-12 19:05:35 -0300

    post Profesores creado

---
+++ Back/src/Controllers/Profesores/postProfesor.js
@@ -0,0 +1,27 @@
+const  {Profesores}  = require('../../db')
+
+
+
+const postProfesor = async(name, apellido, nacionalidad, datebirth, email, username, password) => {
+    try {
+        if (await Profesores.findOne({
+            where: {username: username.toLowerCase()}
+            })
+        ) return {error: `No se pudo completar la carga. Ya existe el username ${username}`}
+
+        const newProfesor = await Profesores.create({
+            name,
+            apellido,
+            nacionalidad,
+            datebirth,
+            email,
+            username,
+            password
+        })
+        return {message: 'Profesor creado con éxito'}
+    } catch (error) {
+        return {error: 'No se pudo agregar el profesor solicitado'}
+    }
+}
+
+module.exports = {postProfesor}
