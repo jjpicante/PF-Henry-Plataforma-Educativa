53c24cafb645191036d472e45468e377d24f61fb
pablo-salituri <pablo_salituri@live.com.ar>
2023-04-12 18:07:28 -0300

    Se creo el post de profesores

--- Back/src/routes/Router/ProfesoresRouter.js
+++ Back/src/routes/Router/ProfesoresRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { getAllProfesores } = require("../../Controllers/Profesores/getAllProfesores");
+const {postProfesor} = require('../../Controllers/Profesores/postProfesor')
 
 
 const profesores = Router();
@@ -8,7 +9,16 @@ profesores.get("/", async(req, res) => {
     const respuesta = await getAllProfesores();
     if (!respuesta.error)
     return res.status(200).json(respuesta)
-    return res.status(500).json(respuesta)
+    return res.status(503).json(respuesta)
 });
+
+
+profesores.post('/', async(req, res) => {
+    const {name, apellido, nacionalidad, datebirth, email, username, password} = req.body;
+    const respuesta = await postProfesor(name, apellido, nacionalidad, datebirth, email, username, password)
+    if (!respuesta.error)
+        return res.status(200).json(respuesta)
+    return res.status(503).json(respuesta)
+})
 
 module.exports = profesores;
