5110b074b650ce8442854d6e1c55277b517b4326
Juan Jose Fiorovic <117408593+jjpicante@users.noreply.github.com>
2023-04-12 13:38:07 -0300

    Redux y BDD harcoded

---
+++ Front/src/Redux/Base de datos HC.js
@@ -0,0 +1,316 @@
+
+export const students =
+    [
+        {
+            "id": 1,
+            "Nombre": "Lucía",
+            "Apellido": "Sánchez",
+            "Nacionalidad": "Española",
+            "Fecha_nacimiento": "2005-09-14",
+            "email": "lucia.sanchez@example.com",
+            "username": "luciasanchez",
+            "password": "contraseña1",
+            "rol": "student"
+        },
+        {
+            "id": 2,
+            "Nombre": "Pablo",
+            "Apellido": "González",
+            "Nacionalidad": "Mexicana",
+            "Fecha_nacimiento": "2007-03-19",
+            "email": "pablo.gonzalez@example.com",
+            "username": "pablogonzalez",
+            "password": "contraseña2",
+            "rol": "student"
+        },
+        {
+            "id": 3,
+            "Nombre": "Carmen",
+            "Apellido": "Rodríguez",
+            "Nacionalidad": "Colombiana",
+            "Fecha_nacimiento": "2006-11-30",
+            "email": "carmen.rodriguez@example.com",
+            "username": "carmenrodriguez",
+            "password": "contraseña3",
+            "rol": "student"
+        },
+        {
+            "id": 4,
+            "Nombre": "Diego",
+            "Apellido": "Hernández",
+            "Nacionalidad": "Español",
+            "Fecha_nacimiento": "2008-06-18",
+            "email": "diego.hernandez@example.com",
+            "username": "diegohernandez",
+            "password": "contraseña4",
+            "rol": "student"
+        },
+        {
+            "id": 5,
+            "Nombre": "Ana",
+            "Apellido": "Martínez",
+            "Nacionalidad": "Argentina",
+            "Fecha_nacimiento": "2009-12-03",
+            "email": "ana.martinez@example.com",
+            "username": "anamartinez",
+            "password": "contraseña5",
+            "rol": "student"
+        },
+        {
+            "id": 6,
+            "Nombre": "Santiago",
+            "Apellido": "López",
+            "Nacionalidad": "Uruguayo",
+            "Fecha_nacimiento": "2005-07-21",
+            "email": "santiago.lopez@example.com",
+            "username": "santiagolopez",
+            "password": "contraseña6",
+            "rol": "student"
+        },
+        {
+            "id": 7,
+            "Nombre": "Carla",
+            "Apellido": "Gómez",
+            "Nacionalidad": "Venezolana",
+            "Fecha_nacimiento": "2010-02-12",
+            "email": "carla.gomez@example.com",
+            "username": "carlagomez",
+            "password": "contraseña7",
+            "rol": "student"
+        },
+        {
+            "id": 8,
+            "Nombre": "Javier",
+            "Apellido": "Herrera",
+            "Nacionalidad": "Chileno",
+            "Fecha_nacimiento": "2007-09-28",
+            "email": "javier.herrera@example.com",
+            "username": "javierherrera",
+            "password": "contraseña8",
+            "rol": "student"
+        },
+        {
+            "id": 9,
+            "Nombre": "Lautaro",
+            "Apellido": "Díaz",
+            "Nacionalidad": "Argentina",
+            "Fecha_nacimiento": "2005-11-23",
+            "email": "lautaro.diaz@example.com",
+            "username": "lautarodiaz",
+            "password": "contraseña9",
+            "rol": "student"
+        },
+        {
+            "id": 10,
+            "Nombre": "Camila",
+            "Apellido": "Fernández",
+            "Nacionalidad": "Uruguaya",
+            "Fecha_nacimiento": "2006-04-02",
+            "email": "camila.fernandez@example.com",
+            "username": "camilafernandez",
+            "password": "contraseña10",
+            "rol": "student"
+        },
+        {
+            "id": 11,
+            "Nombre": "Nicolás",
+            "Apellido": "López",
+            "Nacionalidad": "Colombiano",
+            "Fecha_nacimiento": "2009-10-10",
+            "email": "nicolas.lopez@example.com",
+            "username": "nicolaslopez",
+            "password": "contraseña11",
+            "rol": "student"
+        },
+        {
+            "id": 12,
+            "Nombre": "Florencia",
+            "Apellido": "García",
+            "Nacionalidad": "Chilena",
+            "Fecha_nacimiento": "2008-05-14",
+            "email": "florencia.garcia@example.com",
+            "username": "florenciagarcia",
+            "password": "contraseña12",
+            "rol": "student"
+        },
+        {
+            "id": 13,
+            "Nombre": "Tomás",
+            "Apellido": "Gutiérrez",
+            "Nacionalidad": "Mexicano",
+            "Fecha_nacimiento": "2005-12-19",
+            "email": "tomas.gutierrez@example.com",
+            "username": "tomasgutierrez",
+            "password": "contraseña13",
+            "rol": "student"
+        },
+        {
+            "id": 14,
+            "Nombre": "Isabela",
+            "Apellido": "Martins",
+            "Nacionalidad": "Brasileña",
+            "Fecha_nacimiento": "2007-09-06",
+            "email": "isabela.martins@example.com",
+            "username": "isabelamartins",
+            "password": "contraseña14",
+            "rol": "student"
+        },
+        {
+            "id": 15,
+            "Nombre": "Lucas",
+            "Apellido": "Silva",
+            "Nacionalidad": "Brasileño",
+            "Fecha_nacimiento": "2005-07-15",
+            "email": "lucas.silva@example.com",
+            "username": "lucassilva",
+            "password": "contraseña15",
+            "rol": "student"
+        },
+        {
+            "id": 16,
+            "Nombre": "Valentina",
+            "Apellido": "Moreno",
+            "Nacionalidad": "Colombiana",
+            "Fecha_nacimiento": "2006-12-28",
+            "email": "valentina.moreno@example.com",
+            "username": "valentinamoreno",
+            "password": "contraseña16",
+            "rol": "student"
+        },
+        {
+            "id": 17,
+            "Nombre": "João",
+            "Apellido": "Santos",
+            "Nacionalidad": "Brasileño",
+            "Fecha_nacimiento": "2008-06-02",
+            "email": "joao.santos@example.com",
+            "username": "joaosantos",
+            "password": "contraseña17",
+            "rol": "student"
+        },
+        {
+            "id": 18,
+            "Nombre": "Ana",
+            "Apellido": "González",
+            "Nacionalidad": "Mexicana",
+            "Fecha_nacimiento": "2006-01-20",
+            "email": "ana.gonzalez@example.com",
+            "username": "anagonzalez",
+            "password": "contraseña18",
+            "rol": "student"
+        },
+        {
+            "id": 19,
+            "Nombre": "Luciana",
+            "Apellido": "Vega",
+            "Nacionalidad": "Argentina",
+            "Fecha_nacimiento": "2005-09-12",
+            "email": "luciana.vega@example.com",
+            "username": "lucianavega",
+            "password": "contraseña19",
+            "rol": "student"
+        },
+        {
+            "id": 20,
+            "Nombre": "André",
+            "Apellido": "Garcia",
+            "Nacionalidad": "Brasileño",
+            "Fecha_nacimiento": "2007-07-30",
+            "email": "andre.garcia@example.com",
+            "username": "andregarcia",
+            "password": "contraseña20",
+            "rol": "student"
+        }
+    ]
+
+export const profesors =
+    [
+        {
+            "id": 1,
+            "nombre": "Diego",
+            "apellido": "González",
+            "nacionalidad": "Argentina",
+            "fecha_nacimiento": "1985-12-14",
+            "email": "diego.gonzalez@example.com",
+            "username": "diego.gonzalez",
+            "password": "password123",
+            "rol": "profesor"
+        },
+        {
+            "id": 2,
+            "nombre": "Ana",
+            "apellido": "Martínez",
+            "nacionalidad": "Española",
+            "fecha_nacimiento": "1982-06-29",
+            "email": "ana.martinez@example.com",
+            "username": "ana.martinez",
+            "password": "password456",
+            "rol": "profesor"
+        },
+        {
+            "id": 3,
+            "nombre": "Lucas",
+            "apellido": "Silva",
+            "nacionalidad": "Brasilera",
+            "fecha_nacimiento": "1975-08-08",
+            "email": "lucas.silva@example.com",
+            "username": "lucas.silva",
+            "password": "password789",
+            "rol": "profesor"
+        },
+        {
+            "id": 4,
+            "nombre": "Marcela",
+            "apellido": "Gutiérrez",
+            "nacionalidad": "Mexicana",
+            "fecha_nacimiento": "1988-03-02",
+            "email": "marcela.gutierrez@example.com",
+            "username": "marcela.gutierrez",
+            "password": "password123",
+            "rol": "profesor"
+        },
+        {
+            "id": 5,
+            "nombre": "Rafael",
+            "apellido": "Santos",
+            "nacionalidad": "Colombiana",
+            "fecha_nacimiento": "1978-11-25",
+            "email": "rafael.santos@example.com",
+            "username": "rafael.santos",
+            "password": "password456",
+            "rol": "profesor"
+        },
+        {
+            "id": 6,
+            "nombre": "Alessandra",
+            "apellido": "Costa",
+            "nacionalidad": "Brasilera",
+            "fecha_nacimiento": "1973-04-19",
+            "email": "alessandra.costa@example.com",
+            "username": "alessandra.costa",
+            "password": "password789"
+        },
+        {
+            "id": 7,
+            "nombre": "Juan",
+            "apellido": "Rodríguez",
+            "nacionalidad": "Mexicana",
+            "fecha_nacimiento": "1981-09-12",
+            "email": "juan.rodriguez@example.com",
+            "username": "juan.rodriguez",
+            "password": "password123",
+            "rol": "profesor"
+        },
+        {
+            "id": 8,
+            "nombre": "Sofía",
+            "apellido": "García",
+            "nacionalidad": "Argentina",
+            "fecha_nacimiento": "1976-02-28",
+            "email": "sofia.garcia@example.com",
+            "username": "sofia.garcia",
+            "password": "password456",
+            "rol": "profesor"
+        }
+    ]
+
--- Front/src/Redux/actions.js
+++ Front/src/Redux/actions.js
@@ -1,12 +1,20 @@
 import {
-    GET_STUDENTS
+    GET_STUDENTS,
+    GET_PROFESORS
 } from "./actionsTypes"
-import { students } from "./JsonAlumnos"
+import { profesors, students } from "./Base de datos HC"
 
 
 const getStudents = ()=>{
     return{
         type: GET_STUDENTS,
         payload: students
+    }
+}
+
+const getProfesors = () =>{
+    return{
+        type: GET_PROFESORS,
+        payload: profesors
     }
 }
