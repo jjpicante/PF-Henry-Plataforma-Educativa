2d6f4ed73a405e0fdb0e9abd0cd9ab843892b59c
Ruvaes <113928775+Ruvaes@users.noreply.github.com>
2023-04-13 15:51:02 -0300

    implementacion del logout desde redux

--- Front/src/Components/NavBar/navBar.jsx
+++ Front/src/Components/NavBar/navBar.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { clearUserRole } from '../../Redux/actions';
 import './NavBar.css';
 
 
 function Navbar() {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(clearUserRole());
+  }
 
   return (
     <nav>
-    <div className="nav-container">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/Aulas">Mis Aulas</Link></li>
-        <li><Link to="/Cursos">Mis Cursos</Link></li>
-        <li><Link to="/Logout">Log Out</Link></li>
-      </ul>
-    </div>
-  </nav>
+      <div className="nav-container">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/Aulas">Mis Aulas</Link></li>
+          <li><Link to="/Cursos">Mis Cursos</Link></li>
+          <li><Link to="/" onClick={handleLogout}>Log Out</Link></li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
