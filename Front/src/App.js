27212e4533b07027f180c37e17924dbdeacb84af
IngridLazarte <ingridlazartev@gmail.com>
2023-04-13 16:48:09 -0300

    Home

--- Front/src/App.js
+++ Front/src/App.js
@@ -3,12 +3,9 @@ import { Route, Routes } from 'react-router-dom'
 import Landing from './Components/Landing/landing';
 import HomeStudent from './Components/Home/homeStudent';
 import HomeTeacher from './Components/Home/homeTeacher';
-<<<<<<< HEAD
 import SearchBar from './Components/SearchBar/searchBar';
 import Classroom from './Components/Classroom/Classroom';
-=======
-import Classroom from './Components/Aulas/Aulas';
->>>>>>> e1291b7520e087c41a093ef89fd41dc7d01b972f
+
 
 
 function App() {
@@ -25,11 +22,11 @@ function App() {
 
         <Route exact path='/home' element={<HomeStudent />} />
 
-<<<<<<< HEAD
+
         <Route exact path="/Cursos" element={<Classroom />} />
-=======
+
         <Route exact path="/Aulas" element={<Classroom />} />
->>>>>>> e1291b7520e087c41a093ef89fd41dc7d01b972f
+
 
         <Route exact path="/" element={<Landing />} />
 
