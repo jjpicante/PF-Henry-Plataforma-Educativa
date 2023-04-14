a4faf5b77a3086e26dc79128ca8f5f499dd53b4d
Juan Jose Fiorovic <117408593+jjpicante@users.noreply.github.com>
2023-04-14 11:28:20 -0300

    Cambios en card

--- Front/src/Components/Cards/cards.jsx
+++ Front/src/Components/Cards/cards.jsx
@@ -1,9 +1,10 @@
 import style from "./CardAsignature.module.css"
 
-export default function CardAsignature({ name, id }) {
+export default function CardAsignature({ name, year , id }) {
   return (
     <div className={style.cards}>
       <h1>{name}</h1>
+      <h2>{year}</h2>
     </div>
   );
 }
