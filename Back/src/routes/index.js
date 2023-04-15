e28044ceaf9c9455f0a1fc54aad0eed9c4113794
pablo-salituri <pablo_salituri@live.com.ar>
2023-04-15 15:21:07 -0300

    cambios de prettier

--- Back/src/routes/index.js
+++ Back/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const alumnos = require("./Router/AlumnosRouter");
 const profesores = require("./Router/ProfesoresRouter");
 const admin = require("./Router/AdminRouter");
+const materias = require("./Router/MateriasRouter");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -12,6 +13,7 @@ const router = Router();
 
 router.use("/Alumnos", alumnos);
 router.use("/Profesores", profesores);
+router.use("/Materias", materias);
 router.use("/Admin", admin);
 
 module.exports = router;
