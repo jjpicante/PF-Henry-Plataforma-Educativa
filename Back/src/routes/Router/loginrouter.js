08b2ef38a862c48cab09d590ac006bd5745e26d1
Blacki11 <jeremiasescuela@gmail.com>
2023-04-15 19:16:32 -0300

    Login

--- Back/src/routes/Router/loginrouter.js
+++ Back/src/routes/Router/loginrouter.js
@@ -1,7 +1,14 @@
 const { Router } = require("express");
+const { postLogin } = require("../../Controllers/loggin/postLogin");
 
 const loginrouter = Router();
 
-loginrouter.get("/");
+loginrouter.post("/", async (req, res) => {
+  const { username, password } = req.body;
+
+  const respuesta = await postLogin(password, username);
+  if (!respuesta.error) return res.status(200).json(respuesta);
+  return res.status(503).json(respuesta);
+});
 
 module.exports = loginrouter;
