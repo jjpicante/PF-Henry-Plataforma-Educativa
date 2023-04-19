fe0ed58e14166a02a0d8891984c4f91b8d2179eb
Juan Jose Fiorovic <117408593+jjpicante@users.noreply.github.com>
2023-04-19 13:30:51 -0300

    Pasarela de pago funcionando

--- Back/src/Service/PaymentService.js
+++ Back/src/Service/PaymentService.js
@@ -1,14 +1,17 @@
 const mercadopago = require("mercadopago");
 require("dotenv").config();
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN
+});
 
 class PaymentService {
   async createSubscription(req) {
     const url = "https://api.mercadopago.com/preapproval";
-
+    const meses = req.body.description.join(", ")
     let preference = {
       items: [
         {
-          title: req.body.description,
+          title: meses,
           unit_price: req.body.price,
           quantity: req.body.description.length
         }
@@ -20,25 +23,26 @@ class PaymentService {
         username: req.body.username,
       },
       back_urls: {
-        success: "http://localhost:8080/feedback",
-        failure: "http://localhost:8080/feedback",
-        pending: "http://localhost:8080/feedback"
+        success: "http://localhost:3001/feedback",
+        failure: "http://localhost:3001/feedback",
+        pending: "http://localhost:3001/feedback"
       }
     
   }
 
    // Crear preferencia de pago en Mercado Pago
-  mercadopago.preferences.create(preference)
+  const mp = mercadopago.preferences.create(preference)
+  
   .then(function (response) {
     // Enviar ID de preferencia de pago al cliente
     console.log(response);
-    return({ preferenceId: response.body.id });
+    return(response);
   })
   .catch(function (error) {
     console.log(error);
     return ({ error: 'Error al crear preferencia de pago' });
   });
-  return preference;
+  return mp;
 };
 }
   
