4dc0ac78ac149d2d455955e906b1438353e11a4a
Juan Jose Fiorovic <117408593+jjpicante@users.noreply.github.com>
2023-04-20 10:45:02 -0300

    Arreglo en el ojito

--- Front/src/Components/Forms/validations.js
+++ Front/src/Components/Forms/validations.js
@@ -6,69 +6,39 @@ const numbers = /\d/;
 const validate = (inputs) => {
   let errors = {};
 
-  //Name
+  //Nombre
   if (!inputs.name) {
   } else {
     if (numbers.test(inputs.name))
-      errors.name = "The name must not include numbers!";
+      errors.name = "El nombre no puede incluir numeros";
     if (inputs.name.length > 35)
-      errors.name = "The length of the name must be less than 35 characters";
+      errors.name = "El nombre no puede contener mas de 35 caracteres";
     if (specialChars.test(inputs.name))
-      errors.name = "The name cannot have special characters!";
+      errors.name = "El nombre no puede tener caracteres especiales";
   }
 
-  //Last name
+  //Apellido
   if (!inputs.apellido) {
   } else {
     if (numbers.test(inputs.apellido))
-      errors.apellido = "The last name must not include numbers!";
+      errors.apellido = "El apellido no puede incluir numeros";
     if (inputs.apellido.length > 35)
       errors.apellido =
-        "The length of the last name must be less than 35 characters";
+        "El apellido no puede contener mas de 35 caracteres";
     if (specialChars.test(inputs.apellido))
-      errors.apellido = "The last name cannot have special characters!";
+      errors.apellido = "El apellido no puede tener caracteres especiales";
   }
 
   //Email
   if (!inputs.email) {
   } else {
     if (!emailRegex.test(inputs.email))
-      errors.email = "The email you entered is incorrect";
-    if (inputs.email.length > 35)
-      errors.email = "The email exceeded the 35 available characters";
-  }
-
-  //datebirth
-  /* if (!inputs.datebirth) {
-  } else {
-    if (inputs.datebirth <= 0) errors.datebirth = "The datebirth has to be above 0";
-    if (/\D/.test(inputs.datebirth))
-      errors.datebirth = "Please only enter numeric characters for the datebirth!";
-  } */
-
-  //Subject
-  if (!inputs.subjectName) {
-  } else {
-    if (specialChars.test(inputs.subjectName))
-      errors.subjectName = "The subject cannot have special characters!";
+      errors.email = "El email introducido es incorrecto";
   }
 
   return errors;
 };
 
-const validateSubmit = (student, error) => {
-  for (const property in student) {
-    if (!student[property]) return false;
-  }
-
-  for (const property in error) {
-    if (!error[property]) return false;
-  }
-
-  return true;
-};
-
 module.exports = {
   validate,
-  validateSubmit,
 };
