f008c7e53c63908c400d9b348f659d5f6427c3a7
Roberto Perez Rosales <smile.roberto3@gmail.com>
2023-04-22 07:59:01 -0600

    detalles

--- Back/src/models/Alumnos.js
+++ Back/src/models/Alumnos.js
@@ -46,10 +46,10 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
         defaultValue: "student",
       },
-      // firebaseUserId: {
-      //   type: DataTypes.STRING, // or DataTypes.UUID, depending on how you store the UID in Firebase
-      //   allowNull: true, // or false if you want to make it required
-      // },
+      firebaseUserId: {
+        type: DataTypes.STRING, // or DataTypes.UUID, depending on how you store the UID in Firebase
+        allowNull: true, // or false if you want to make it required
+      },
       anio: {
         type: DataTypes.STRING,
         allowNull: true,
