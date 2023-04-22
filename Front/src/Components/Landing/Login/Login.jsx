4398d81ec8f7845efde6bf90098ec07f1e78d54e
Ruvaes <113928775+Ruvaes@users.noreply.github.com>
2023-04-22 12:07:53 -0300

    a

--- Front/src/Components/Landing/Login/Login.jsx
+++ Front/src/Components/Landing/Login/Login.jsx
@@ -1,33 +1,37 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import "./Login.css"
-import { postlogin } from '../../../Redux/actions';
+import './landing.css';
+import { postlogin } from '../../Redux/actions';
+
 
 function Login() {
   const dispatch = useDispatch();
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
   const handleLogin = async (event) => {
     event.preventDefault();
+
     //buscan en la base de datos si el usuario existe
+
     try {
+      if (!email || !password) {
+        setErrorMessage("Please enter both email and password");
+        return;
+      }
       // busca en la base de datos si el usuario existe
-      const userData = await dispatch(postlogin(username, password));
-  
-      if (userData && userData.message) {
-        setErrorMessage(userData.message);
-      } else {
-        // hacer update del estado con el rol del login
-        localStorage.setItem("userRole", userData.role);
-        // después de hacer el update lo redirige al home
+      const userData = await dispatch(postlogin(email, password));
+      if(userData){
         window.location.href = "/home";
+      } else {
+        setErrorMessage("Error al iniciar sesión");
       }
     } catch (error) {
       setErrorMessage("Error al iniciar sesión");
     }
   }
+
 
   return (
     <div id="landing">
@@ -35,14 +39,15 @@ function Login() {
         <form className='form' onSubmit={handleLogin}>
           <h2>Iniciar Sesion</h2>
           <div className="user-box">
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <label>Usuario</label>
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <label>Email</label>
           </div>
           <div className="user-box">
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             <label>Contraseña</label>
           </div>
           <button type="submit">Iniciar Sesion</button>
+          <br></br>
         </form>
         <p style={{ color: "red" }}>{errorMessage}</p>
       </div>
