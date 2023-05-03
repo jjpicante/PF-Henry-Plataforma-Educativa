91bd3a352a2b488cad3bc770c489a7c9b7e02cee
Blacki11 <jeremiasescuela@gmail.com>
2023-05-03 16:43:48 -0300

    Desarrollo

--- Front/src/Components/Landing/Desarrolladores/desarrolladores.jsx
+++ Front/src/Components/Landing/Desarrolladores/desarrolladores.jsx
@@ -6,6 +6,8 @@ import Ingrid from "./Ingrid.jpg";
 import Juan from "./Juan.jpg";
 import compañero from "./compañero.jpg";
 
+import Jere from "./jere.jpg";
+
 export default function Desarrolladores() {
   const navigate = useNavigate();
   return (
@@ -13,13 +15,12 @@ export default function Desarrolladores() {
       <h1>Desarrolladores</h1>
       <h2>Te damos la bienvenida!</h2>
       <p>
-        Somos el Grupo 5 del proyecto final de carrera de Henry, conformado por
-        seis Full Stack Developers; quienes a largo lo del presente proyecto,
-        hemos puesto en práctica todos nuestros conocimientos y habilidades
-        blandas incorporadas durante la carrera, dentro de un ambiente
-        comfortable, positivo y de ideas constructivas; a fin de lograr un
-        producto final altamente satisfactorio tanto para nosotros como para los
-        usuarios de la Plataforma Estudiantil.
+        Somos el Grupo 5 del proyecto final de carrera de Henry, conformado por seis Full Stack
+        Developers; quienes a largo lo del presente proyecto, hemos puesto en práctica todos
+        nuestros conocimientos y habilidades blandas incorporadas durante la carrera, dentro de un
+        ambiente comfortable, positivo y de ideas constructivas; a fin de lograr un producto final
+        altamente satisfactorio tanto para nosotros como para los usuarios de la Plataforma
+        Estudiantil.
       </p>
       <h3>Desplazate hacia abajo y conocenos un poco mas!</h3>
       <br />
@@ -44,17 +45,16 @@ export default function Desarrolladores() {
             </div>
             <p>Tengo 33 años y nací en Tandil, Argentina. </p>
             <p>
-              Convencido del valor del aprendizaje, siempre me he preocupado por
-              formarme y adquirir constantemente nuevas habilidades: Soy Técnico
-              en Informática, Ingeniero Civil, y futuro Full Stack Developer;
-              además de contar con estudios en Inglés, Francés y Alemán.
+              Convencido del valor del aprendizaje, siempre me he preocupado por formarme y adquirir
+              constantemente nuevas habilidades: Soy Técnico en Informática, Ingeniero Civil, y
+              futuro Full Stack Developer; además de contar con estudios en Inglés, Francés y
+              Alemán.
             </p>
 
             <p>
-              Disfruto mucho programar, y sobre todo la libertad que brinda
-              poder hacer mi trabajo desde cualquier sitio; lo que resulta ideal
-              para alguien que ama viajar y conocer lugares nuevos todo el
-              tiempo.
+              Disfruto mucho programar, y sobre todo la libertad que brinda poder hacer mi trabajo
+              desde cualquier sitio; lo que resulta ideal para alguien que ama viajar y conocer
+              lugares nuevos todo el tiempo.
             </p>
           </div>
         </section>
@@ -78,18 +78,16 @@ export default function Desarrolladores() {
             </div>
             <p>Tengo 21 años y soy de Córdoba, Argentina. </p>
             <p>
-              Desde que tengo uso de razón, he buscado oportunidades para
-              ampliar mi conocimiento en distintas áreas. He estudiado música,
-              soy preceptora en nivel secundario y también me desempeño como
-              peluquera canina. Actualmente, estoy cursando la carrera de
-              Ingeniería Industrial.
+              Desde que tengo uso de razón, he buscado oportunidades para ampliar mi conocimiento en
+              distintas áreas. He estudiado música, soy preceptora en nivel secundario y también me
+              desempeño como peluquera canina. Actualmente, estoy cursando la carrera de Ingeniería
+              Industrial.
             </p>
 
             <p>
-              Motivada por mi interés en adquirir nuevos conocimientos y por el
-              afán de seguir incursionando en nuevas disciplinas, me interesé
-              por la programación. Creo que esta disciplina ofrece muchas
-              posibilidades y puedo utilizarla como complemento para mi
+              Motivada por mi interés en adquirir nuevos conocimientos y por el afán de seguir
+              incursionando en nuevas disciplinas, me interesé por la programación. Creo que esta
+              disciplina ofrece muchas posibilidades y puedo utilizarla como complemento para mi
               formación universitaria.
             </p>
           </div>
@@ -112,29 +110,25 @@ export default function Desarrolladores() {
               </svg>
               <p>jjfiorovic@gmail.com</p>
             </div>
+            <p>Tengo 32 años. Soy de Gualeguaychu, Entre Rios, Argentina.</p>
             <p>
-              Tengo 32 años. Soy de Gualeguaychu, Entre
-              Rios, Argentina.
-            </p>
-            <p>
-              Aunque soy Preparador Físico de formación, siempre he tenido una
-              pasión por la tecnología. Ahora quiero centrarme en mi pasión
-              tecnológica y hacer del deporte un hobby. Durante mi formación
-              como programador he aprendido a estar muy enfocado en los
-              objetivos, lo cual me ha ayudado a estar preparado para cualquier
-              desafío que se me presente en el futuro.
+              Aunque soy Preparador Físico de formación, siempre he tenido una pasión por la
+              tecnología. Ahora quiero centrarme en mi pasión tecnológica y hacer del deporte un
+              hobby. Durante mi formación como programador he aprendido a estar muy enfocado en los
+              objetivos, lo cual me ha ayudado a estar preparado para cualquier desafío que se me
+              presente en el futuro.
             </p>
 
             <p>
-              Me emociona pensar dónde me llevará mi futuro como desarrollador y
-              estoy ansioso por seguir aprendiendo y creciendo en mi carrera.
+              Me emociona pensar dónde me llevará mi futuro como desarrollador y estoy ansioso por
+              seguir aprendiendo y creciendo en mi carrera.
             </p>
           </div>
         </section>
         <section className={styles.integrante}>
-          <img className={styles.img} src={compañero} alt="compañero" />
+          <img className={styles.img} src={Jere} alt="Jeremias" />
           <div className={styles.textos}>
-            <h3 className={styles.nombre}>Meñi</h3>
+            <h3 className={styles.nombre}>Jeremias De Miguel</h3>
             <div className={styles.mail}>
               <svg
                 className={styles.sobre}
@@ -147,12 +141,23 @@ export default function Desarrolladores() {
               >
                 <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
               </svg>
-              <p>pablo_salituri@live.com.ar</p>
+              <p>jeremiasdemiguel08@gmail.com</p>
             </div>
-            <p>Que mira bobo</p>
-            <p>Que mira bobo</p>
+            <p>
+              Soy Jeremías de Miguel, un apasionado desarrollador de software de 19 años. Completé
+              un curso en Herny y actualmente estudio Desarrollo de Software en la Universidad
+              Nacional de Cuyo. Con mi equipo, desarrollamos con éxito una página web eficiente y
+              fácil de usar.
+            </p>
+            <p>
+              Mi amor por la tecnología va más allá del desarrollo de software; disfruto explorando
+              nuevos programas y herramientas. Entreno y me divierto con la cultura japonesa y los
+              videojuegos. Aunque puedo ser impaciente, siempre manejo mis emociones para no afectar
+              mi trabajo. La música es una de mis pasiones y siempre busco mejorar mis habilidades y
+              aprender nuevas tecnologías. ¡Estoy emocionado de mostrar todo lo que he aprendido!
+            </p>
 
-            <p>Anda, anda palla bobo</p>
+            <p></p>
           </div>
         </section>
         <section className={styles.integrante}>
